49e2a1af70d874c3578b2d9c645b724f2f6683c1
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-11-29 22:26:31 +0300

    reformat: remove unused imports

--- src/main/java/artgallery/user/repository/UserRepository.java
+++ src/main/java/artgallery/user/repository/UserRepository.java
@@ -2,7 +2,6 @@ package artgallery.user.repository;
 
 import artgallery.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
