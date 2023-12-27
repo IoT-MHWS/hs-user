433e8a3387eb8060f063a52bb283bb1db1f89695
thermvs <mariagewitter@gmail.com>
2023-12-28 00:30:33 +0300

    feat: add kafka functionality

---
+++ src/main/java/artgallery/user/dto/MessageDTO.java
@@ -0,0 +1,16 @@
+package artgallery.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDTO {
+  @JsonProperty("mail")
+  private String mail;
+}
