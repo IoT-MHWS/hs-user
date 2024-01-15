30de4fdaf1da47c58cc5ba135b590dd978c29184
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-01-15 16:27:21 +0300

    refactor: to match clean architecture

--- src/main/java/artgallery/user/service/AuthService.java
+++ src/main/java/artgallery/user/service/AuthService.java
@@ -2,52 +2,8 @@ package artgallery.user.service;
 
 import artgallery.user.dto.TokenDTO;
 import artgallery.user.dto.UserLoginDTO;
-import artgallery.user.dto.UserRegisterDTO;
-import artgallery.user.entity.UserEntity;
-import artgallery.user.exception.UserDoesNotExistException;
-import artgallery.user.repository.UserRepository;
-import artgallery.user.configuration.jwt.JwtService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
-import java.util.stream.Collectors;
-
-@Service
-@RequiredArgsConstructor
-public class AuthService {
-
-  private final UserRepository userRepository;
-  private final ReactiveAuthenticationManager authenticationManager;
-  private final JwtService jwtService;
-
-  public Mono<TokenDTO> login(UserLoginDTO req) {
-    return authenticationManager
-      .authenticate(new UsernamePasswordAuthenticationToken(req.getLogin(), req.getPassword()))
-      .publishOn(Schedulers.boundedElastic())
-      .map(auth -> userRepository.findByLogin(req.getLogin()))
-      .flatMap(optional -> optional.map(this::generateTokenFromEntity)
-        .orElseGet(() -> Mono.error(new UserDoesNotExistException(req.getLogin()))));
-  }
-
-  private Mono<TokenDTO> generateTokenFromEntity(UserEntity user) {
-    return Mono.just(jwtService.generateToken(User
-      .withUsername(user.getLogin())
-      .password(user.getPassword())
-      .authorities(user.getRoles().stream()
-        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
-        .collect(Collectors.toList()))
-      .accountLocked(false)
-      .accountExpired(false)
-      .credentialsExpired(false)
-      .disabled(false)
-      .build()));
-  }
-
-
+public interface AuthService {
+  Mono<TokenDTO> login(UserLoginDTO req);
 }
--- src/main/java/artgallery/user/service/UserService.java
+++ src/main/java/artgallery/user/service/UserService.java
@@ -2,165 +2,17 @@ package artgallery.user.service;
 
 import artgallery.user.configuration.ServerUserDetails;
 import artgallery.user.dto.*;
-import artgallery.user.entity.RoleEntity;
-import artgallery.user.entity.UserEntity;
-import artgallery.user.exception.RoleAlreadyExistsException;
-import artgallery.user.exception.RoleDoesNotExistException;
-import artgallery.user.exception.UserAlreadyExistsException;
-import artgallery.user.exception.UserDoesNotExistException;
-import artgallery.user.repository.RoleRepository;
-import artgallery.user.repository.UserRepository;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+public interface UserService {
+  Mono<UserCreatedDTO> create(UserRegisterDTO userDTO);
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-  @Autowired
-  private KafkaTemplate<String, EmailDTO> kafkaTemplate;
+  Mono<UserDetailsDTO> getUserDetails(ServerUserDetails userDetails);
 
-  private final UserRepository userRepository;
-  private final RoleRepository roleRepository;
-  private final PasswordEncoder passwordEncoder;
+  Mono<Void> addRole(String login, Role role);
 
-  public Mono<UserCreatedDTO> create(UserRegisterDTO userDTO) {
-    return Mono.just(userDTO)
-      .subscribeOn(Schedulers.boundedElastic())
-      .<UserEntity>handle((dto, sink) -> {
-        if (userRepository.existsByLogin(dto.getLogin())) {
-          sink.error(new UserAlreadyExistsException(dto.getLogin()));
-          return;
-        }
+  Mono<Void> removeRole(String login, Role role);
 
-        var userEntity = UserEntity.builder()
-          .login(dto.getLogin())
-          .password(passwordEncoder.encode(dto.getPassword()))
-          .email(dto.getEmail())
-          .roles(new ArrayList<>())
-          .build();
-
-        var roleEntity = roleRepository.findByName(Role.PUBLIC.name());
-
-        if (roleEntity.isEmpty()) {
-          sink.error(new RoleDoesNotExistException(Role.PUBLIC));
-          return;
-        }
-
-        userEntity.getRoles().add(roleEntity.get());
-        userRepository.save(userEntity);
-        sink.next(userEntity);
-
-        EmailDTO emailDTO = new EmailDTO();
-        emailDTO.setEmail(dto.getEmail());
-        emailDTO.setLogin(dto.getLogin());
-        emailDTO.setPassword(dto.getPassword());
-        kafkaTemplate.send("email", emailDTO);
-      }).map(entity -> new UserCreatedDTO(entity.getId(), entity.getLogin(), entity.getEmail()));
-  }
-
-  public Mono<UserDetailsDTO> getUserDetails(ServerUserDetails userDetails) {
-    return Mono.just(new UserDetailsDTO(
-      userDetails.getId(),
-      userDetails.getUsername(),
-      userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-    );
-  }
-
-  public Mono<Void> addRole(String login, Role role) {
-    return Mono.just(login)
-      .subscribeOn(Schedulers.boundedElastic())
-      .<UserEntity>handle((val, sink) -> {
-        try {
-          UserEntity entity = addRoleBlocking(login, role);
-          sink.next(entity);
-        } catch (Exception ex) {
-          sink.error(ex);
-        }
-      }).then();
-  }
-
-  @Transactional
-  private UserEntity addRoleBlocking(String login, Role role) {
-    var userEntityOpt = userRepository.findByLogin(login);
-    if (userEntityOpt.isEmpty()) {
-      throw new UserDoesNotExistException(login);
-    }
-    var roleEntityOpt = roleRepository.findByName(role.name());
-    if (roleEntityOpt.isEmpty()) {
-      throw new RoleDoesNotExistException(role);
-    }
-
-    var userEntity = userEntityOpt.get();
-    var roleEntity = roleEntityOpt.get();
-
-    if (userEntity.getRoles().contains(roleEntity)) {
-      throw new RoleAlreadyExistsException(role);
-    }
-
-    userEntity.getRoles().add(roleEntity);
-    roleRepository.save(roleEntity);
-    return userRepository.save(userEntity);
-  }
-
-  public Mono<Void> removeRole(String login, Role role) {
-    return Mono.just(login)
-      .subscribeOn(Schedulers.boundedElastic())
-      .<UserEntity>handle((val, sink) -> {
-        try {
-          UserEntity entity = removeRoleBlocking(login, role);
-          sink.next(entity);
-        } catch (Exception ex) {
-          sink.error(ex);
-        }
-      }).then();
-  }
-
-  @Transactional
-  private UserEntity removeRoleBlocking(String login, Role role) {
-    var userEntityOpt = userRepository.findByLogin(login);
-    if (userEntityOpt.isEmpty()) {
-      throw new UserDoesNotExistException(login);
-    }
-    var roleEntityOpt = roleRepository.findByName(role.name());
-    if (roleEntityOpt.isEmpty()) {
-      throw new RoleDoesNotExistException(role);
-    }
-
-    var userEntity = userEntityOpt.get();
-    var roleEntity = roleEntityOpt.get();
-
-    if (!userEntity.getRoles().contains(roleEntity)) {
-      throw new RoleDoesNotExistException(role);
-    }
-
-    userEntity.getRoles().remove(roleEntity);
-    return userRepository.save(userEntity);
-  }
-
-  public Flux<RoleDTO> getRoles(String login) {
-    return Mono.just(login)
-      .subscribeOn(Schedulers.boundedElastic())
-      .<List<RoleEntity>>handle((val, sink) -> {
-        var userEntityOpt = userRepository.findByLogin(login);
-        if (userEntityOpt.isEmpty()) {
-          sink.error(new UserDoesNotExistException(login));
-          return;
-        }
-        sink.next(userEntityOpt.get().getRoles());
-      })
-      .flatMapMany(Flux::fromIterable)
-      .map(entity -> new RoleDTO(Role.valueOf(entity.getName())));
-  }
+  Flux<RoleDTO> getRoles(String login);
 }
